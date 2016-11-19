e067ab757b5d02f3b8979a397ed51a72724091be
David Koblas <david@tubularlabs.com>
2016-11-19 15:14:38 -0800

    New post script

---
+++ new_post.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import time
+import readline
+
+BASE_DIR = 'content/post/'
+
+files = sorted([file for file in os.listdir(BASE_DIR) if file != '.'])
+
+post_id = 0
+with open(os.path.join(BASE_DIR, files[-1]), 'rb') as fd:
+    for line in fd:
+        if line.startswith('url:'):
+            post_id = int(line.split('/')[-1])
+
+if post_id == 0:
+    print("Unable to find post_id")
+    exit(1)
+
+title = None
+while not title:
+    title = raw_input('Title for post: ')
+
+slug = title.lower().replace(' ', '-')
+
+template = '''---
+title: %(title)s
+author: koblas
+layout: post
+date: %(date)s
+url: /p/%(slug)s/%(post_id)s
+categories:
+  - draft
+
+---
+
+# TEXT HERE
+'''
+
+tnow = time.gmtime()
+tstr = time.strftime('%Y-%m-%dT%H:%M:%S+00:00', tnow)
+output = os.path.join(BASE_DIR, '%s-%s.md' % (time.strftime('%Y-%m-%d', tnow), slug))
+
+with open(output, 'wb') as fd:
+    fd.write(template % { 'title': title, 'slug': slug, 'post_id': post_id, 'date': tstr })
+
+print "OUTPUT = ", output
